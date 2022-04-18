2d5e92374deb4e16bd3067645e44fe8a1e54b0a6
Jahid11186 <zahidhasansamim@gmail.com>
2022-04-18 12:55:03 +0600

    Created: General information update and Password changing functionality

---
+++ src/components/Settings/Settings.js
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import Sidebar from '../Nav/Sidebar/Sidebar'
+import ChangePassword from './ChangePassword/ChangePassword'
+import General from './General/General'
+import classes from './Settings.module.css'
+
+const Settings = () => {
+    const [menu, setMenu] = useState(1)
+
+    return (
+        <div className={classes.Settings}>
+            <div>
+                <Sidebar />
+            </div>
+            <div className={classes.Wrapper}>
+                <div>
+                    <div className={classes.Nav}>
+                        <span
+                            className={menu === 1 ? `${classes.activeNav}` : `${classes.deactiveNav}`}
+                            onClick={(e) => setMenu(1)}>
+                            General
+                        </span>
+                        {/* <span
+                            className={menu === 2 ? `${classes.activeNav}` : `${classes.deactiveNav}`}
+                            onClick={(e) => setMenu(2)}>
+                            User Detail
+                        </span>
+                        <span
+                            className={menu === 3 ? `${classes.activeNav}` : `${classes.deactiveNav}`}
+                            onClick={(e) => setMenu(3)}>
+                            Patient information
+                        </span> */}
+                        <span
+                            className={menu === 3 ? `${classes.activeNav}` : `${classes.deactiveNav}`}
+                            onClick={(e) => setMenu(3)}>
+                            Change Password
+                        </span>
+                    </div>
+                    <div>
+                        {menu === 1 ? <General /> : null}
+                        {/* {menu === 2 ? <UserDetail /> : null}
+                        {menu === 3 ? <PatientInfo /> : null} */}
+                        {menu === 3 ? <ChangePassword /> : null}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Settings
