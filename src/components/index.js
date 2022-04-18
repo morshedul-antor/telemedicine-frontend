07e9de03108c5e645cbaaec95d10fefb32d46d64
Jahid11186 <zahidhasansamim@gmail.com>
2022-04-18 13:05:51 +0600

    imported: Settings imported

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Dashboard from './Dashboard/Dashboard'
 import Login from './Login/Login'
 import Profile from './Profile/Profile'
 import Register from './Register/Register'
+import Settings from './Settings/Settings'
 
-export { Login, Register, Profile, Dashboard }
+export { Login, Register, Profile, Dashboard, Settings }
