d2e2db3282ab715803c549726035cc4071985247
Jahid11186 <zahidhasansamim@gmail.com>
2022-04-18 13:02:53 +0600

    fetched: Information fetched for user profile and chamber informations

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,15 +1,68 @@
+import { useContext, useEffect, useState } from 'react'
+import { Auth } from '../../allContext'
 import { Sidebar } from '../Nav'
+import ProfileHeader from '../ProfileHeader/ProfileHeader'
+import Chambers from './Chambers/Chambers'
 import classes from './Profile.module.css'
+import ProfileCard from './ProfileCard/ProfileCard'
 
 const Profile = () => {
+    const [doctorDetail, setDoctorDetail] = useState({})
+    const [chamberInfo, setChamberInfo] = useState({})
+
+    const { stateAuth } = useContext(Auth)
+    const apiV1 = process.env.REACT_APP_API_V1
+    const token = stateAuth.token
+
+    useEffect(() => {
+        let infoFunc = async () => {
+            let infoFetch = await fetch(`${apiV1}/doctors/auth`, {
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+                method: 'GET',
+            })
+            let infoJson = await infoFetch.json()
+
+            if (infoFetch.ok) {
+                setDoctorDetail(infoJson)
+            }
+        }
+        try {
+            infoFunc()
+        } catch (e) {}
+    }, [apiV1, token])
+
+    useEffect(() => {
+        let chamberFunc = async () => {
+            let chamberFetch = await fetch(`${apiV1}/doctors/chamber/`, {
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+                method: 'GET',
+            })
+            let chamberJson = await chamberFetch.json()
+            if (chamberFetch.ok) {
+                setChamberInfo(chamberJson)
+            }
+        }
+        try {
+            chamberFunc()
+        } catch (e) {}
+    }, [apiV1, token])
     return (
         <div className={classes.Profile}>
             <div>
                 <Sidebar />
             </div>
-
             <div>
-                <h2>Hello</h2>
+                <ProfileHeader userDetail={doctorDetail} />
+                <ProfileCard />
+                <Chambers chamberDetail={chamberInfo} />
             </div>
         </div>
     )
