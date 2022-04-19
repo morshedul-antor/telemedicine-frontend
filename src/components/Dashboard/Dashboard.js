ddc2a4f97c1dfba9329b709d6d0c58d6875bdcda
Jahid11186 <zahidhasansamim@gmail.com>
2022-04-20 00:30:07 +0600

    ApiFetched: Information from doctor/auth shows in Dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,7 @@
-import { faHouseChimneyMedical } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Snapshot, Appointment } from '.'
-import docCover from '../../assets/img/background-doc-table.jpg'
-import doc from '../../assets/img/docstock.jpg'
 import { Sidebar } from '../Nav'
 import ProfileHeader from '../ProfileHeader/ProfileHeader'
+import DoctorDetails from '../Settings/DoctorDetails/DoctorDetails'
 import classes from './Dashboard.module.css'
 
 const Dashboard = () => {
@@ -14,38 +11,7 @@ const Dashboard = () => {
                 <Sidebar />
             </div>
             <div>
-                <div
-                    className={classes.header}
-                    style={{
-                        background: `url(${docCover})`,
-                        backgroundPosition: 'center',
-                        backgroundSize: 'cover',
-                    }}>
-                    <div>
-                        <div className={classes.headLeftWrapper}>
-                            <div
-                                style={{
-                                    background: `url(${doc})`,
-                                    width: '150px',
-                                    height: '150px',
-                                    backgroundPosition: 'center',
-                                    backgroundSize: 'cover',
-                                    borderRadius: '10px',
-                                    border: '4px solid var(--white)',
-                                    boxShadow: `0 3px 5px var(--grey2)`,
-                                }}></div>
-                            <h2>Rashadul Hassan</h2>
-                            <p>BMDC: A-2345</p>
-                        </div>
-                    </div>
-                    <div>
-                        <FontAwesomeIcon icon={faHouseChimneyMedical} />
-                        <h2>Chamber</h2>
-                        <p>Bananni L block</p>
-                        <p>26 no. house</p>
-                        <p>Dhaka</p>
-                    </div>
-                </div>
+                <ProfileHeader userDetail={DoctorDetails} />
                 <Snapshot />
                 <Appointment />
             </div>
