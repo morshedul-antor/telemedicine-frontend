39e88873d479dad3a0a10771e5afb672d9e18424
Jahid11186 <zahidhasansamim@gmail.com>
2022-04-21 13:38:55 +0600

    Clean code

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -20,16 +20,6 @@ const Settings = () => {
                             onClick={(e) => setMenu(1)}>
                             General
                         </span>
-                        {/* <span
-                            className={menu === 2 ? `${classes.activeNav}` : `${classes.deactiveNav}`}
-                            onClick={(e) => setMenu(2)}>
-                            User Detail
-                        </span>
-                        <span
-                            className={menu === 3 ? `${classes.activeNav}` : `${classes.deactiveNav}`}
-                            onClick={(e) => setMenu(3)}>
-                            Patient information
-                        </span> */}
                         <span
                             className={menu === 3 ? `${classes.activeNav}` : `${classes.deactiveNav}`}
                             onClick={(e) => setMenu(3)}>
@@ -38,8 +28,6 @@ const Settings = () => {
                     </div>
                     <div>
                         {menu === 1 ? <General /> : null}
-                        {/* {menu === 2 ? <UserDetail /> : null}
-                        {menu === 3 ? <PatientInfo /> : null} */}
                         {menu === 3 ? <ChangePassword /> : null}
                     </div>
                 </div>
