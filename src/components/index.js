d3574a9e3618def17189f4799ae898c15f8f64bd
Jahid11186 <zahidhasansamim@gmail.com>
2022-04-21 13:41:57 +0600

    Schedule page included  to make exportable to App.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ import Dashboard from './Dashboard/Dashboard'
 import Login from './Login/Login'
 import Profile from './Profile/Profile'
 import Register from './Register/Register'
+import Schedule from './Schedule/Schedule'
 import Settings from './Settings/Settings'
 
-export { Login, Register, Profile, Dashboard, Settings }
+export { Login, Register, Profile, Dashboard, Settings, Schedule }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import HomePage from './HomePage'
 import LoginPage from './LoginPage'
 import ProfilePage from './ProfilePage'
 import RegisterPage from './RegisterPage'
+import SchedulePage from './SchedulePage'
 import SettingsPage from './SettingsPage'
 
-export { HomePage, ProfilePage, LoginPage, RegisterPage, DashboardPage, SettingsPage }
+export { HomePage, ProfilePage, LoginPage, RegisterPage, DashboardPage, SettingsPage, SchedulePage }
