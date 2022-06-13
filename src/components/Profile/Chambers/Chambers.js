5baedc171868f74da73d8ff5b8bedc2b7adf664d
Jahid11186 <zahidhasansamim@gmail.com>
2022-06-13 18:13:51 +0600

    Chamber Activation/Deactivation changed

--- src/components/Profile/Chambers/Chambers.js
+++ src/components/Profile/Chambers/Chambers.js
@@ -17,6 +17,7 @@ const Chambers = () => {
 
     const [name, setName] = useState('')
     const [detail, setDetail] = useState('')
+    const [active, setActive] = useState({})
 
     const apiV1 = process.env.REACT_APP_API_V1
     const token = stateAuth.token
@@ -58,7 +59,6 @@ const Chambers = () => {
             if (chamberFetch.ok) {
                 setChamberInfo(chamberJson)
             }
-            console.log(chamberInfo)
         }
         try {
             chamberFunc()
@@ -66,6 +66,41 @@ const Chambers = () => {
     }, [apiV1, token, msg])
 
     let data = Array.from(chamberInfo)
+
+    // Activate and Active Chamber
+
+    // const activateChamberFunc = async (e) => {
+    //     e.preventDefault()
+    //     let activateChamberFetch = await fetch(`${apiV1}/doctors/chamber/activate/${chamberId}`, {
+    //         headers: {
+    //             Authorization: `Bearer ${token}`,
+    //         },
+    //         method: 'PUT',
+    //     })
+    //     if (activateChamberFetch.ok) {
+    //         setMsg(...msg, 'Chamber Activated')
+    //     }
+    // }
+
+    useEffect(() => {
+        let activeChamberFunc = async (e) => {
+            let activeChamberFetch = await fetch(`${apiV1}/doctors/chamber/active`, {
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+                method: 'GET',
+            })
+            let activeChamberJson = await activeChamberFetch.json()
+            if (activeChamberFetch.ok) {
+                setActive(activeChamberJson)
+            }
+        }
+        try {
+            activeChamberFunc()
+        } catch (e) {}
+    }, [apiV1, token, msg])
 
     return (
         <div className={classes.ChamberPage}>
@@ -79,6 +114,19 @@ const Chambers = () => {
                     <p>Action</p>
                 </div>
                 {data.map((chamber, i) => {
+                    // API fetch for Activating Chamber
+                    const activateChamberFunc = async (e) => {
+                        e.preventDefault()
+                        let activateChamberFetch = await fetch(`${apiV1}/doctors/chamber/activate/${chamber.id}`, {
+                            headers: {
+                                Authorization: `Bearer ${token}`,
+                            },
+                            method: 'PUT',
+                        })
+                        if (activateChamberFetch.ok) {
+                            setMsg(...msg, 'Chamber Activated')
+                        }
+                    }
                     return (
                         <div className={classes.body} key={i}>
                             <p>{i + 1}</p>
@@ -86,7 +134,12 @@ const Chambers = () => {
                             <p>District</p>
                             <p>{chamber.detail}</p>
                             <p>
-                                <ChamberState chamberId={chamber.id} />
+                                <ChamberState
+                                    chamberId={chamber.id}
+                                    activate={activateChamberFunc}
+                                    msg={msg}
+                                    active={active}
+                                />
                             </p>
                             <p>
                                 <div className={classes.btn}>
