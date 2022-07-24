eac264e2397859b1f31aa2a00c5bb1a20d9bf862
Jahid11186 <zahidhasansamim@gmail.com>
2022-07-24 18:25:15 +0600

    new menu: reordering menus

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Sidebar from '../Nav/Sidebar/Sidebar'
 import ChangePassword from './ChangePassword/ChangePassword'
 import General from './General/General'
+import OtherInfo from './OtherInfo/OtherInfo'
 import ProfessionalInfoEdit from './ProfessionalInfoEdit/ProfessionalInfoEdit'
 import classes from './Settings.module.css'
 import SpecialityQualificationEdit from './SpecialityQualificationEdit/SpecialityQualificationEdit'
@@ -25,24 +26,30 @@ const Settings = () => {
                         <span
                             className={menu === 2 ? `${classes.activeNav}` : `${classes.deactiveNav}`}
                             onClick={(e) => setMenu(2)}>
-                            Professional Info
+                            Fees & Experience
                         </span>
                         <span
                             className={menu === 3 ? `${classes.activeNav}` : `${classes.deactiveNav}`}
                             onClick={(e) => setMenu(3)}>
-                            Speciality
+                            Professional Info
                         </span>
                         <span
                             className={menu === 4 ? `${classes.activeNav}` : `${classes.deactiveNav}`}
                             onClick={(e) => setMenu(4)}>
+                            Speciality
+                        </span>
+                        <span
+                            className={menu === 5 ? `${classes.activeNav}` : `${classes.deactiveNav}`}
+                            onClick={(e) => setMenu(5)}>
                             Change Password
                         </span>
                     </div>
                     <div>
                         {menu === 1 ? <General /> : null}
-                        {menu === 2 ? <ProfessionalInfoEdit /> : null}
-                        {menu === 3 ? <SpecialityQualificationEdit /> : null}
-                        {menu === 4 ? <ChangePassword /> : null}
+                        {menu === 2 ? <OtherInfo /> : null}
+                        {menu === 3 ? <ProfessionalInfoEdit /> : null}
+                        {menu === 4 ? <SpecialityQualificationEdit /> : null}
+                        {menu === 5 ? <ChangePassword /> : null}
                     </div>
                 </div>
             </div>
