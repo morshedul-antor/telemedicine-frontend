2e6881f65e48f83d26cb67d0977418cd555a1f17
Jahid11186 <zahidhasansamim@gmail.com>
2022-07-26 13:55:10 +0600

    remove component: dashboard2

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-import Dashboard2 from './Dashboard2/Dashboard2'
 import Dashboard from './Dashboard/Dashboard'
 import Login from './Login/Login'
 import Profile from './Profile/Profile'
@@ -6,4 +5,4 @@ import Register from './Register/Register'
 import Schedule from './Schedule/Schedule'
 import Settings from './Settings/Settings'
 
-export { Login, Register, Profile, Dashboard, Settings, Schedule, Dashboard2 }
+export { Login, Register, Profile, Dashboard, Settings, Schedule }
