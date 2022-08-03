7324addb52b3777ad6072bbcd67ae030a5460b7d
Jahid11186 <zahidhasansamim@gmail.com>
2022-08-03 16:42:23 +0600

    clean: unused code

--- src/components/Profile/Chambers/ChamberState/ChamberState.js
+++ src/components/Profile/Chambers/ChamberState/ChamberState.js
@@ -3,47 +3,6 @@
 import classes from './ChamberState.module.css'
 
 const ChamberState = ({ chamberId, activate, active }) => {
-    // const { stateAuth } = useContext(Auth)
-
-    // const [msg, setMsg] = useState([])
-    // const [active, setActive] = useState({})
-
-    // const apiV1 = process.env.REACT_APP_API_V1
-    // const token = stateAuth.token
-
-    // const activateChamberFunc = async (e) => {
-    //     e.preventDefault()
-    //     let activateChamberFetch = await fetch(`${apiV1}/doctors/chamber/activate/${chamberId}`, {
-    //         headers: {
-    //             Authorization: `Bearer ${token}`,
-    //         },
-    //         method: 'PUT',
-    //     })
-    //     if (activateChamberFetch.ok) {
-    //         setMsg(...msg, 'Chamber Activated')
-    //     }
-    // }
-
-    // useEffect(() => {
-    //     let activeChamberFunc = async (e) => {
-    //         let activeChamberFetch = await fetch(`${apiV1}/doctors/chamber/active`, {
-    //             headers: {
-    //                 Accept: 'application/json',
-    //                 'Content-Type': 'application/json',
-    //                 Authorization: `Bearer ${token}`,
-    //             },
-    //             method: 'GET',
-    //         })
-    //         let activeChamberJson = await activeChamberFetch.json()
-    //         if (activeChamberFetch.ok) {
-    //             setActive(activeChamberJson)
-    //         }
-    //     }
-    //     try {
-    //         activeChamberFunc()
-    //     } catch (e) {}
-    // }, [apiV1, token, msg])
-
     return (
         <label className={classes.switch}>
             <button onClick={activate}></button>
