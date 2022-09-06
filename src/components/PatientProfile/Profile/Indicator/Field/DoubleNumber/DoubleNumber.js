8dfcb36c9895d8a6179d0a9b89a9dfd4aa2b4d5c
morshedul-antor <antor.morshedul@gmail.com>
2022-09-06 17:18:08 +0600

    feat: patient health indicator component add

---
+++ src/components/PatientProfile/Profile/Indicator/Field/DoubleNumber/DoubleNumber.js
@@ -0,0 +1,47 @@
+import classes from './DoubleNumber.module.css'
+
+const DoubleNumber = (props) => {
+    return (
+        <div className={classes.DoubleNumber}>
+            <div className={classes.header}>
+                <p>{props.title}</p>
+            </div>
+            <div className={classes.body}>
+                <div>{props.children}</div>
+                <div className={classes.field}>
+                    <form>
+                        <div className={classes.divRef}>
+                            <p>{props.ref1}</p>
+                            <li>{props.ref2}</li>
+                            <li>{props.ref3}</li>
+                        </div>
+                        <div>
+                            <input
+                                type="number"
+                                placeholder={props.place1}
+                                value={props.st1}
+                                onChange={(e) => props.setSt1(e.target.value)}
+                                min={props.min1}
+                                max={props.max1}
+                            />
+                            <input
+                                type="number"
+                                placeholder={props.place2}
+                                value={props.st2}
+                                onChange={(e) => props.setSt2(e.target.value)}
+                                min={props.min2}
+                                max={props.max2}
+                            />
+                            <span>{props.unit}</span>
+                        </div>
+                        <div>
+                            <button onClick={props.smbt}>Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DoubleNumber
---
+++ src/components/PatientProfile/Profile/Indicator/Pulse.js
@@ -0,0 +1,95 @@
+import { useContext, useState, useEffect } from 'react'
+import { Auth } from '../../../../allContext'
+import { toMonthNameShort } from '../../../../utils/date'
+import { LineChart } from '../../../Chart'
+import { Number } from './index'
+
+const Pulse = () => {
+    const { stateAuth } = useContext(Auth)
+
+    const [pulse, setPulse] = useState()
+    const [dataPulse, setDataPulse] = useState([])
+
+    const apiV1 = process.env.REACT_APP_API_V1
+
+    let token = stateAuth.token
+
+    const submit = async (e) => {
+        e.preventDefault()
+
+        let pulseFetch = await fetch(`${apiV1}/patient/indicators`, {
+            headers: {
+                Accept: 'appllication/json',
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+            dataType: 'json',
+            method: 'POST',
+            body: JSON.stringify({
+                key: 'pulse',
+                unit: 'rate/minute',
+                slot_int1: pulse,
+            }),
+        })
+
+        // let pulseJson = await pulseFetch.json()
+
+        if (pulseFetch.ok) {
+            setPulse(0)
+        }
+    }
+
+    useEffect(() => {
+        let pulseFunc = async () => {
+            let pulseFetch = await fetch(`${apiV1}/patient/indicators/pulse`, {
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+                method: 'GET',
+            })
+
+            let pulseJson = await pulseFetch.json()
+
+            if (pulseFetch.ok) {
+                setDataPulse(pulseJson)
+            }
+        }
+
+        try {
+            pulseFunc()
+        } catch (e) {}
+    }, [apiV1, token, pulse])
+
+    let data = {
+        labels: [
+            ...dataPulse
+                .map(
+                    (elm) =>
+                        `${elm.created_at.slice(8, 10)}-${toMonthNameShort(
+                            elm.created_at.slice(6, 7)
+                        )}${elm.created_at.slice(2, 4)}`
+                )
+                .reverse(),
+        ],
+        datasets: [
+            {
+                label: 'Pulse',
+                data: [...dataPulse.map((elm) => elm.slot_int1).reverse()],
+                fill: true,
+                backgroundColor: 'rgba(245, 66, 66,0.2)',
+                borderColor: 'rgba(245, 66, 66,1)',
+                lineTension: 0.4,
+            },
+        ],
+    }
+
+    return (
+        <div>
+            <LineChart data={data} />
+        </div>
+    )
+}
+
+export default Pulse
