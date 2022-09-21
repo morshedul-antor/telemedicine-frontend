a170236f0f502d0f357e3ee801ff66341f6692db
morshedul-antor <antor.morshedul@gmail.com>
2022-09-21 18:20:56 +0600

    remove: alert message

--- src/components/ScheduleV2/ScheduleForm/ScheduleForm.jsx
+++ src/components/ScheduleV2/ScheduleForm/ScheduleForm.jsx
@@ -71,7 +71,6 @@ export default function ScheduleForm({ apiV1, token, chambers }) {
         })
         if (schedulePost.ok) {
             // setMsg([...msg, 'New Chamber added.'])
-            alert('success')
             refreshPage()
         }
     }
