de986621193126d1144967a7924163d7b9613b8b
morshedul-antor <antor.morshedul@gmail.com>
2022-09-21 18:17:54 +0600

    export: schedule component

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,8 @@ import Login from './Login/Login'
 import PatientProfile from './PatientProfile/PatientProfile'
 import Profile from './Profile/Profile'
 import Register from './Register/Register'
-import Schedule from './Schedule/Schedule'
+// import Schedule from './Schedule/Schedule'
+import Schedule from './ScheduleV2/Schedule'
 import Settings from './Settings/Settings'
 
 export { Login, Register, Profile, Dashboard, Settings, Schedule, Ecard, PatientProfile }
