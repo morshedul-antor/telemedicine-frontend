025248f1c0fae49c39a22255fae4df8f6181e13c
morshedul-antor <antor.morshedul@gmail.com>
2022-10-31 17:39:03 +0600

    fix: mobile responsive

--- src/components/LandingPage/Pricing/Pricing.jsx
+++ src/components/LandingPage/Pricing/Pricing.jsx
@@ -1,33 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import Popup from './Popup/Popup'
 import classes from './Pricing.module.css'
 
 export default function Pricing() {
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
-        <div className={classes.wrapper}>
+        <div className={classes.wrapper} id="subscription">
+            <div className={classes.logo}>
+                <span>-----</span> Subscription Plan <span>-----</span>
+            </div>
             <div className={classes.pricingTable}>
                 <ul className={classes.gridFour}>
                     <li className={classes.table}>
                         <h1 className={classes.tableHeader}>Basic</h1>
                         <p className={classes.tablePricing}>
-                            <span>৳</span>
-                            <span>0</span>
-                            <span>/for 1 month</span>
+                            <div>
+                                <span>৳</span>
+                                <span>0</span>
+                                <span>/for 1 month</span>
+                            </div>
                         </p>
                         <ul className={classes.tableOptions}>
                             <li>Doctor Digital Profile</li>
                             <li>Doctor Promotion in Doctor Portal</li>
+                            <p>Free Trail For 1 Month</p>
+                            <li>Doctor Schedule Management</li>
+                            <li>Patient Online Appointment</li>
+                            <li>Patient Serial Management</li>
+                            <li>Patient Health Record</li>
+                            <li>E-Prescription</li>
+                            <li>Patient List</li>
                         </ul>
-                        <Link href="https://user.healthxbd.com/">
+                        <Link to="login">
                             <button className={classes.tableStart}>Subscribe Now</button>
                         </Link>
                     </li>
                     <li className={classes.table}>
                         <h1 className={classes.tableHeader}>Plus</h1>
                         <p className={classes.tablePricing}>
-                            <span>৳</span>
-                            <span>999</span>
-                            <span>/for 1 month</span>
+                            <div>
+                                <span>৳</span>
+                                <span>999</span>
+                                <span>/for 1 month</span>
+                            </div>
+                            <div>
+                                <span>৳</span>
+                                <span>4999</span>
+                                <span>/for 1 year</span>
+                            </div>
                         </p>
                         <ul className={classes.tableOptions}>
                             <li>Doctor Digital Profile</li>
@@ -39,11 +61,14 @@ export default function Pricing() {
                             <li>E-Prescription</li>
                             <li>Patient List</li>
                         </ul>
-                        <Link href="https://user.healthxbd.com/">
-                            <button className={classes.tableStart}>Subscribe Now</button>
+                        <Link href="#">
+                            <button className={classes.tableStart} onClick={() => setIsOpen(true)}>
+                                Subscribe Now
+                            </button>
                         </Link>
                     </li>
                 </ul>
+                {isOpen && <Popup setIsOpen={setIsOpen} />}
             </div>
         </div>
     )
