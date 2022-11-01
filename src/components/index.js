2b45e9a09cab18bd73ead2df2dadf1190eea73a8
morshedul-antor <antor.morshedul@gmail.com>
2022-11-01 17:49:46 +0600

    feat: patient list component add

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ import Chambers from './Chambers/Chambers'
 import Dashboard from './Dashboard/Dashboard'
 import Ecard from './Ecard/Ecard'
 import Login from './Login/Login'
+import Patient from './Patient/Patient'
 import PatientProfile from './PatientProfile/PatientProfile'
 import Profile from './Profile/Profile'
 import Register from './Register/Register'
@@ -9,4 +10,4 @@ import Register from './Register/Register'
 import Schedule from './ScheduleV2/Schedule'
 import Settings from './Settings/Settings'
 
-export { Login, Register, Profile, Dashboard, Settings, Schedule, Ecard, PatientProfile, Chambers }
+export { Login, Register, Profile, Dashboard, Settings, Schedule, Ecard, PatientProfile, Chambers, Patient }
---
+++ src/pages/PatientPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Patient } from '../components'
+
+export default function PatientPage() {
+    return (
+        <div>
+            <Patient />
+        </div>
+    )
+}
