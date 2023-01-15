4c03c0d7e2e3ae92a5f2429c4d677c0b5dab7441
morshedul-antor <antor.morshedul@gmail.com>
2023-01-15 14:10:44 +0600

    feat: ep list add with view option

--- src/components/Patient/Patient.jsx
+++ src/components/Patient/Patient.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { Auth } from '../../allContext'
 import { Sidebar } from '../Nav'
 import ProfileHeader from '../ProfileHeader/ProfileHeader'
 import classes from './Patient.module.css'
 import PatientList from './PatientList/PatientList'
 
 export default function Patient() {
+    const [patients, setPatients] = useState([])
+    const { stateAuth } = useContext(Auth)
+    const token = stateAuth.token
+    const apiV1 = process.env.REACT_APP_API_V1
+
+    useEffect(() => {
+        let fetchData = async () => {
+            let response = await fetch(`${apiV1}/ep/doctor/ep/?skip=0&limit=10`, {
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+                method: 'GET',
+            })
+            let data = await response.json()
+
+            if (response.ok) {
+                setPatients(data)
+                console.log('data', data)
+            }
+        }
+        try {
+            fetchData()
+        } catch (e) {
+            setPatients([])
+        }
+    }, [apiV1, token])
+
     return (
         <div className={classes.patient}>
             <div>
@@ -13,7 +43,7 @@ export default function Patient() {
             <div>
                 <ProfileHeader />
                 <br />
-                <PatientList />
+                <PatientList patients={patients} />
             </div>
         </div>
     )
--- src/components/Patient/PatientList/PatientList.jsx
+++ src/components/Patient/PatientList/PatientList.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Table from '../../Resource/Table/Table'
 import classes from './PatientList.module.css'
 
-export default function PatientList() {
+export default function PatientList({ patients }) {
+    const epV1 = process.env.REACT_APP_EP
+
     return (
         <div className={classes.list}>
             <Table>
@@ -10,25 +13,33 @@ export default function PatientList() {
                     <tr>
                         <th>Sl.</th>
                         <th>Patient Name</th>
+                        <th>Patient Sex</th>
                         <th>Cause of Consultation</th>
                         <th>Date</th>
-                        <th>Remarks</th>
-                        <th>Profile</th>
+                        <th>Patient Profile</th>
+                        <th>Prescription</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {/* <tr className={classes.row}>
-                        <td>1</td>
-                        <td>Antor</td>
-                        <td>Headache</td>
-                        <td>02-10-2022</td>
-                        <td>Eat Properly</td>
-                        <td>
-                            <a href="">
-                                <button>Click</button>
-                            </a>
-                        </td>
-                    </tr> */}
+                    {patients[1]?.map((ep, index) => (
+                        <tr className={classes.row}>
+                            <td>{index + 1}</td>
+                            <td>{ep.patient_name || '-'}</td>
+                            <td>{ep.patient_sex || '-'}</td>
+                            <td>{ep.cause_of_consultation}</td>
+                            <td>{ep.created_at !== null ? ep.created_at.slice(0, 10) : '-'}</td>
+                            <td>
+                                <a href={`/patient/${ep.patient_id}`}>
+                                    <button>Click</button>
+                                </a>
+                            </td>
+                            <td>
+                                <a href={`${epV1}/ep/hxep${ep.id + 100000}`}>
+                                    <button>View</button>
+                                </a>
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </Table>
         </div>
