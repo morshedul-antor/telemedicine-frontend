4e3352bc2ea36e1caec7e73050d35d3d5a52a5f5
morshedul-antor <antor.morshedul@gmail.com>
2023-02-14 12:16:53 +0600

    fix: prescription list

--- src/components/Patient/Patient.jsx
+++ src/components/Patient/Patient.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { Auth } from '../../allContext'
+import { dateTime } from '../../utils/date'
 import { Sidebar } from '../Nav'
 import ProfileHeader from '../ProfileHeader/ProfileHeader'
 import classes from './Patient.module.css'
@@ -8,12 +9,15 @@ import PatientList from './PatientList/PatientList'
 export default function Patient() {
     const [patients, setPatients] = useState([])
     const { stateAuth } = useContext(Auth)
+
     const token = stateAuth.token
     const apiV1 = process.env.REACT_APP_API_V1
 
+    const currentDate = dateTime.slice(0, 10)
+
     useEffect(() => {
         let fetchData = async () => {
-            let response = await fetch(`${apiV1}/ep/doctor/?skip=0&limit=100`, {
+            let response = await fetch(`${apiV1}/ep/doctor/?end_date=${currentDate}T23%3A59%3A59&skip=0&limit=100`, {
                 headers: {
                     Accept: 'application/json',
                     'Content-Type': 'application/json',
@@ -32,7 +36,7 @@ export default function Patient() {
         } catch (e) {
             setPatients([])
         }
-    }, [apiV1, token])
+    }, [apiV1, token, currentDate])
 
     return (
         <div className={classes.patient}>
