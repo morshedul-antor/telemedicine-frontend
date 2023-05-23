dcd7582b10b69e6de354eafc719afa14c67fb874
morshedul-antor <antor.morshedul@gmail.com>
2023-05-23 14:29:36 +0600

    fix: code

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import { MobileNav } from '../Nav'
 import classes from './Layout.module.css'
 
 export default function Layout({ children, side }) {
     return (
         <div className={classes.layout}>
-            {/* <MobileNav /> */}
             <div>{children}</div>
         </div>
     )
